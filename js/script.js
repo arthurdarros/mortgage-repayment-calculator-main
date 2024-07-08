cc2ab82093b4d1daa3ddd9c932577e8a6f6584ad
Arthur Stein Darros <arthurdarros@gmail.com>
2024-07-08 19:28:06 -0300

    css fixes

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function calculate(){
+    let morAmount = Number(document.getElementById('amount').value);
+    let morTerm = Number(document.getElementById('term').value);
+    let intRate = Number(document.getElementsById('rate').value);
+    let morType = document.querySelector('input[name="mortgage"]:checked').value;
+
+
+}
